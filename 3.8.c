6e69033f4739d451a77dc311f5e92aaa93d48b9d
frankxff <1227291519@qq.com>
2019-01-02 20:18:48 +0800

    typesize.c

---
+++ 3.8.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ 
+ int main()
+ {
+ 	printf("%zd bytes\n",sizeof(int));
+ 	printf("%zd bytes\n",sizeof(char));
+ 	printf("%zd bytes\n",sizeof(long));
+ 	printf("%zd bytes\n",sizeof(long long));
+ 	printf("%zd bytes\n",sizeof(long double));
+ 	printf("%zd bytes\n",sizeof(double));
+ 	
+ 	return 0;
+ }
