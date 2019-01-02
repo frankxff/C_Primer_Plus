056b8a75fa117048ec2f322ed330e268bf30783d
frankxff <1227291519@qq.com>
2019-01-02 20:47:13 +0800

    escape.c

---
+++ 3.10.c
@@ -0,0 +1,13 @@
+ #include<stdio.h>
+
+int main()
+{
+	float salary;
+	printf("\aEnter your desired monthly salary:");
+	printf("$_______\b\b\b\b\b\b\b");
+	scanf("%f",&salary);
+	printf("\n\t$%.2f a month is $%.2f a year.",salary);
+	printf("\rGee!\n");
+	
+	return 0;
+}
