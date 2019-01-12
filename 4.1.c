1b3597d4d5fa4e27c880387cf83dc21d796a0a58
frankxff <1227291519@qq.com>
2019-01-12 15:10:05 +0800

    talkback.c

---
+++ 4.1.c
@@ -0,0 +1,32 @@
+/*
+//talkback.c 
+//show the interactive with users演示与用户交互 
+*/
+
+#include<stdio.h>
+#include<string.h>  // providing the func strlen()提供strlen()的函数原型
+#define DENSITY 62.4 //define human density 人体密度
+
+int main()
+{
+	float weight, volume;
+	int size, letters;
+	char name[40];     //define a arry name containing 40 characters *name是一个可容纳40个字符的数组 
+	
+	printf("Hi, what's your first name?\n");
+	scanf("%s", name);
+	printf("%s, what's your weight in pounds?\n",name);
+	scanf("%f",&weight);
+	size = sizeof name;
+	letters = strlen(name);
+	volume = weight/DENSITY;
+	printf("Well,%s,your volume is %2.2f cubic feet.\n",name,volume);
+	printf("Also,your first name has %d letters,\n",letters);
+	printf("and we have %d bytes to store it.\n",size);	
+	
+	
+	
+	
+	return 0;
+}
+
